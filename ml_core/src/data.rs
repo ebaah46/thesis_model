92e5e8d26d2338ab2dce9a1ad5a3a1827faadcc7
BEKs <ebaah46@yahoo.com>
2026-07-07 15:39:40 +0300

    Update data.rs

--- ml_core/src/data.rs
+++ ml_core/src/data.rs
@@ -1,9 +1,11 @@
 // Should export data preprocessing API
 
+pub mod batcher;
 pub mod dataset;
 pub mod preprocessing;
 pub mod source;
 
+pub use crate::data::batcher::TrainingBatch;
 pub use crate::data::dataset::{Dataset, RawItem};
 pub use crate::data::preprocessing::{OHEStrategy, Preprocessing};
 pub use crate::data::source::{RawRecord, load_records_from_csv};
